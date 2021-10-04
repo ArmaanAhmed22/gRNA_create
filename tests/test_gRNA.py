34796342be4a099a0acea467ce29e14275afa023
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2021-10-03 21:49:33 -0400

    Formatting and added negative sequence test case

--- tests/test_gRNA.py
+++ tests/test_gRNA.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import pytest
-from Bio import SeqIO
-
 from gRNA_create.gRNA import gRNA_Factory
 from gRNA_create.gRNA_scorer import Scorer, TwoPenaltyScorer
 from gRNA_create.pam import PAM, End
 from gRNA_create.utils import f_score
+from tests.conf import negative_seqs, positive_seqs
 
 
 @pytest.mark.parametrize("gRNA_PAM, gRNA_length, scorer, scoring_metric, expected", [
@@ -21,10 +20,8 @@ def test_factory_create_gRNAs_positive(
         scoring_metric,
         expected: int,
 ):
-    with open("tests/test_sequences.fasta", "r") as h:
-        positive_seqs = [str(record.seq) for record in SeqIO.parse(h, format="fasta")]
-        results: pd.DataFrame = gRNA_Factory(gRNA_PAM, gRNA_length, scorer).create_gRNAs(positive_seqs, scoring_metric)
-        assert results.shape[0] == expected
+    results: pd.DataFrame = gRNA_Factory(gRNA_PAM, gRNA_length, scorer).create_gRNAs(positive_seqs, scoring_metric)
+    assert results.shape[0] == expected
 
 
 @pytest.mark.parametrize("gRNA_PAM, gRNA_length, scorer, scoring_metric, expected", [
@@ -40,9 +37,22 @@ def test_factory_create_gRNAs_positive_and_rev(
         scoring_metric,
         expected: int,
 ):
-    with open("tests/test_sequences.fasta", "r") as h:
-        positive_seqs = [str(record.seq) for record in SeqIO.parse(h, format="fasta")]
-        results: pd.DataFrame = gRNA_Factory(gRNA_PAM, gRNA_length, scorer). \
-            create_gRNAs_with_reverse_complement(positive_seqs, scoring_metric)
-        print(results)
-        assert results.shape[0] == expected
+    results: pd.DataFrame = gRNA_Factory(gRNA_PAM, gRNA_length, scorer). \
+        create_gRNAs_with_reverse_complement(positive_seqs, scoring_metric)
+    print(results)
+    assert results.shape[0] == expected
+
+
+@pytest.mark.parametrize("gRNA_PAM, gRNA_length, scorer, scoring_metric, expected", [
+    (PAM(End(5), "TT"), 14, TwoPenaltyScorer(), f_score(3), "AGCTAGACATGACA")
+])
+def test_factory_create_gRNAs_pos_neg(
+        gRNA_PAM: PAM,
+        gRNA_length: int,
+        scorer: Scorer,
+        scoring_metric,
+        expected: str,
+):
+    results: pd.DataFrame = gRNA_Factory(gRNA_PAM, gRNA_length, scorer). \
+        create_gRNAs(positive_seqs, scoring_metric, genomes_miss=negative_seqs)
+    assert results.sort_values(by=scoring_metric.__name__, ascending=False).gRNA.iloc[0].spacer_eq(expected)
